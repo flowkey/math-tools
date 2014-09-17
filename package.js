445e55e40e8f5187d87c84b900b0252f6346871d
Ahmed Hassan <a.hassan@buismed.de>
2014-09-17 16:48:32 +0200

    removed meteor dep

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
   summary: "Some optimized mathematical operations for your meteor packages",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/flowkey/math-tools.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.2.1');
   api.addFiles('flowkey:math-tools.js','client');
   api.add_files(['flashsort.js'], 'client');
 
