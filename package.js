3f0653320e40eba5038375490c0a89582f00c779
Ahmed Hassan <a.hassan@buismed.de>
2014-09-17 16:39:45 +0200

    removed test files

--- package.js
+++ package.js
@@ -1,16 +1,24 @@
 Package.describe({
-  summary: " \* Fill me in! *\ ",
-  version: "1.0.0",
-  git: " \* Fill me in! *\ "
+  summary: "Some optimized mathematical operations for your meteor packages",
+  version: "0.0.1",
+  git: "https://github.com/flowkey/math-tools.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.1');
-  api.addFiles('flowkey:math-tools.js');
-});
+  api.addFiles('flowkey:math-tools.js','client');
+  api.add_files(['flashsort.js'], 'client');
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('flowkey:math-tools');
-  api.addFiles('flowkey:math-tools-tests.js');
+    api.export(['zArray',
+     'getMaxOfArray',
+     'copyArray',
+     'getMinOfArray',
+     'getSumOfArray',
+     'getBin',
+     'getFreq',
+     'mean',
+     'median',
+     'variance',
+     'standardDeviation',
+     'createLinearSpace']);
 });
